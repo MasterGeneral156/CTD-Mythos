b347a0038aae1d6bb732a871235da0ed696439d3
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-24 22:36:23 -0400

    Add config option for Human Effigy

---
+++ src/main/java/com/themastergeneral/ctdmythos/common/config/ModConfig.java
@@ -0,0 +1,38 @@
+package com.themastergeneral.ctdmythos.common.config;
+
+import net.minecraftforge.common.config.Configuration;
+
+import org.apache.logging.log4j.Level;
+
+import com.themastergeneral.ctdmythos.CTDMythos;
+import com.themastergeneral.ctdmythos.proxy.Common;
+
+public class ModConfig {
+	private static final String CATEGORY_GENERAL = "General";
+	
+	//Actual Config shit
+	public static int DurabilityHumanEffigy = 5;
+	public static void readConfig() {
+		Configuration cfg = Common.config;
+		try {
+			cfg.load();
+			initGeneralConfig(cfg);
+		} catch (Exception e1) {
+			CTDMythos.logger.log(Level.ERROR, "Problem loading config file!",
+					e1);
+		} finally {
+			if (cfg.hasChanged()) {
+				cfg.save();
+			}
+		}
+	}
+
+	private static void initGeneralConfig(Configuration cfg) {
+		cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration for our mod.");
+		
+		//Config for maximum uses on Human Effigy
+		DurabilityHumanEffigy = cfg.getInt("Human Effigy Max Uses", CATEGORY_GENERAL,
+				DurabilityHumanEffigy, 1, 32766,
+				"How many uses per effigy");
+	}
+}
