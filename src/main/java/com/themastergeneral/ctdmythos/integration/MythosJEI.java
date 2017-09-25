b454f95448774ea2800116d79570508e07089dcd
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-24 22:38:17 -0400

    Working on JEI Plugin

---
+++ src/main/java/com/themastergeneral/ctdmythos/integration/MythosJEI.java
@@ -0,0 +1,17 @@
+package com.themastergeneral.ctdmythos.integration;
+
+import com.themastergeneral.ctdmythos.common.items.CowBloodItem;
+import com.themastergeneral.ctdmythos.common.items.ModItems;
+
+import net.minecraft.item.ItemStack;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+
+@JEIPlugin
+public class MythosJEI implements IModPlugin {
+	@Override
+	public void register(IModRegistry registry) {
+		registry.addDescription(new ItemStack(ModItems.cowblooddrop), "jei.cowblooddrop.desc");
+	}
+}
