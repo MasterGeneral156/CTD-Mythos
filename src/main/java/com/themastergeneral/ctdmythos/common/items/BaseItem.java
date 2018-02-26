019ff8163dc5cbac534d1be4da305b611e46960f
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-02-25 20:27:08 -0500

    Crystal Memory repairs tools in offhand

--- src/main/java/com/themastergeneral/ctdmythos/common/items/BaseItem.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/BaseItem.java
@@ -2,9 +2,14 @@ package com.themastergeneral.ctdmythos.common.items;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
 import com.themastergeneral.ctdcore.item.CTDItem;
@@ -38,5 +43,44 @@ public class BaseItem extends CTDItem {
 		if (stack.getItem() == ModItems.crystal_fire) {
 			((EntityLivingBase) entityIn).extinguish();
 		}
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn,
+			EntityPlayer playerIn, EnumHand handIn) {
+		ItemStack offhand = playerIn.getHeldItemOffhand();
+		ItemStack mainhand = playerIn.getHeldItemMainhand();
+		//Crystalized Memory to repair the item in the user's offhand, at
+		//the cost of one Crystalized Memory.
+		if (mainhand.getItem() == ModItems.crystal_memory)
+		{
+			if (offhand != ItemStack.EMPTY)
+			{
+				if (offhand.isItemDamaged())
+				{
+					offhand.setItemDamage(0);
+					mainhand.shrink(1);
+				}
+			}
+		}
+		//Crystal oath + book in offhand to get a tome of XP
+		if (mainhand.getItem() == ModItems.crystal_oath)
+		{
+			if (offhand != ItemStack.EMPTY)
+			{
+				if (offhand.getItem() == Items.BOOK)
+				{
+					if (playerIn.experienceLevel >= 5)
+					{
+						playerIn.addExperienceLevel(-5);
+						offhand.shrink(1);
+						mainhand.shrink(1);
+						playerIn.dropItem(new ItemStack(ModItems.xptome), false);
+					}
+				}
+			}
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.PASS,
+				playerIn.getHeldItem(handIn));
 	}
 }
