d98e4330c4a53b76fa5f3c066917a185bf0c5089
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-02-26 15:42:23 -0500

    Ethereal Fiber from shearing spiders

--- src/main/java/com/themastergeneral/ctdmythos/common/items/ShearsItem.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/ShearsItem.java
@@ -7,6 +7,7 @@ import com.themastergeneral.ctdmythos.common.items.base.BaseItem;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntitySpider;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -42,6 +43,15 @@ public class ShearsItem extends BaseItem {
 				if (itemdrop <= 5)
 					player.dropItem(new ItemStack(ModItems.crystal_ender), true);
 			}
+		}
+		// Ethereal mob drop. Would be phantoms in 1.13 tho
+		if (target instanceof EntitySpider) {
+			stack.damageItem(1, attacker);
+			// Random Number Generator!
+			Random randomGenerator = new Random();
+			int itemdrop = randomGenerator.nextInt(100);
+			if (itemdrop <= 10)
+				player.dropItem(new ItemStack(ModItems.ethereal_fiber), true);
 		}
 		return false;
 	}
