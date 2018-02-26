51e7f9b553b4cf8481aada7235c40e9d3c8a3bc3
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-02-26 14:21:44 -0500

    Added archeron ingot

---
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/ArcheronIngot.java
@@ -0,0 +1,15 @@
+package com.themastergeneral.ctdmythos.common.items;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public class ArcheronIngot extends BaseItem {
+
+	public ArcheronIngot(String name, String modid) {
+		super(name, modid);
+	}
+}
