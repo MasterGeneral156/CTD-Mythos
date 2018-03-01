0c5a44ecb36e3fa42a83b989e51b720d5b2e089e
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-02-28 19:24:06 -0500

    Added the removal >.>

--- src/main/java/com/themastergeneral/ctdmythos/common/processing/WandFlightItems.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/processing/WandFlightItems.java
@@ -12,57 +12,57 @@ import com.google.common.collect.Maps;
 
 public class WandFlightItems {
 	private static final WandFlightItems FLIGHT_ITEMS = new WandFlightItems();
-	
-	//Flight Item + Flight Time
+
+	// Flight Item + Flight Time
 	private final Map<ItemStack, Integer> flightItemsList = Maps
 			.<ItemStack, Integer> newHashMap();
-	
-	//Flight Item + Flight Multiplier
+
+	// Flight Item + Flight Multiplier
 	private final Map<ItemStack, Integer> flightMultiList = Maps
 			.<ItemStack, Integer> newHashMap();
-	
-	//Flight Item + Resistance Time
-		private final Map<ItemStack, Integer> flightResistList = Maps
-				.<ItemStack, Integer> newHashMap();
-			
+
+	// Flight Item + Resistance Time
+	private final Map<ItemStack, Integer> flightResistList = Maps
+			.<ItemStack, Integer> newHashMap();
+
 	public static WandFlightItems instance() {
 		return FLIGHT_ITEMS;
 	}
-	
-	private WandFlightItems()
-	{
+
+	private WandFlightItems() {
 		this.addFlight(new ItemStack(Items.DIAMOND), 300, 1, 360);
 		this.addFlight(new ItemStack(Items.EMERALD), 400, 0, 480);
 		this.addFlight(new ItemStack(Items.IRON_INGOT), 80, 0, 120);
 		this.addFlight(new ItemStack(Items.GOLD_INGOT), 40, 3, 70);
-		
+
 		this.addFlight(new ItemStack(Blocks.DIAMOND_BLOCK), 3000, 1, 3600);
 		this.addFlight(new ItemStack(Blocks.EMERALD_BLOCK), 4000, 0, 4800);
 		this.addFlight(new ItemStack(Blocks.IRON_BLOCK), 800, 0, 1200);
 		this.addFlight(new ItemStack(Blocks.GOLD_BLOCK), 400, 3, 700);
-		
+
 		this.addFlight(new ItemStack(Items.IRON_NUGGET), 8, 0, 12);
 		this.addFlight(new ItemStack(Items.GOLD_NUGGET), 4, 3, 7);
 	}
-	
-	public void addFlight(ItemStack input, int flighttime, int flightmultiplier, int resistancetime)
-	{
+
+	public void addFlight(ItemStack input, int flighttime,
+			int flightmultiplier, int resistancetime) {
 		this.addFlightItem(input, flighttime, flightmultiplier, resistancetime);
 	}
-	
-	public void addFlightItem(ItemStack input, int flighttime, int flightmultiplier, int resistancetime)
-	{
+
+	public void addFlightItem(ItemStack input, int flighttime,
+			int flightmultiplier, int resistancetime) {
 		if (getFlightItem(input) != ItemStack.EMPTY) {
-			CTDMythos.logger.error("Ignored flight item with conflicting input: " + input );
+			CTDMythos.logger
+					.error("Ignored flight item with conflicting input: "
+							+ input);
 			return;
 		}
 		this.flightItemsList.put(input, flighttime);
 		this.flightMultiList.put(input, flightmultiplier);
 		this.flightResistList.put(input, resistancetime);
 	}
-	
-	public ItemStack getFlightItem(ItemStack stack)
-	{
+
+	public ItemStack getFlightItem(ItemStack stack) {
 		for (Entry<ItemStack, Integer> entry : this.flightItemsList.entrySet()) {
 			if (this.compareItemStacks(stack, (ItemStack) entry.getKey())) {
 				return (ItemStack) entry.getKey();
@@ -70,19 +70,18 @@ public class WandFlightItems {
 		}
 		return ItemStack.EMPTY;
 	}
-	
+
 	private boolean compareItemStacks(ItemStack stack1, ItemStack stack2) {
 		return stack2.getItem() == stack1.getItem()
 				&& (stack2.getMetadata() == 32767 || stack2.getMetadata() == stack1
 						.getMetadata());
 	}
-	
+
 	public Map<ItemStack, Integer> getFlightItemsList() {
 		return this.flightItemsList;
 	}
-	
-	public int getFlightTime(ItemStack stack)
-	{
+
+	public int getFlightTime(ItemStack stack) {
 		for (Entry<ItemStack, Integer> entry : this.flightItemsList.entrySet()) {
 			if (this.compareItemStacks(stack, (ItemStack) entry.getKey())) {
 				return (entry.getValue()).intValue();
@@ -90,9 +89,8 @@ public class WandFlightItems {
 		}
 		return 0;
 	}
-	
-	public int getFlightMultiplier(ItemStack stack)
-	{
+
+	public int getFlightMultiplier(ItemStack stack) {
 		for (Entry<ItemStack, Integer> entry : this.flightMultiList.entrySet()) {
 			if (this.compareItemStacks(stack, (ItemStack) entry.getKey())) {
 				return (entry.getValue()).intValue();
@@ -100,14 +98,25 @@ public class WandFlightItems {
 		}
 		return 0;
 	}
-	
-	public int getFlightResistance(ItemStack stack)
-	{
+
+	public int getFlightResistance(ItemStack stack) {
 		for (Entry<ItemStack, Integer> entry : this.flightResistList.entrySet()) {
 			if (this.compareItemStacks(stack, (ItemStack) entry.getKey())) {
 				return (entry.getValue()).intValue();
 			}
 		}
 		return 0;
+	}
+
+	public void removeFlight(ItemStack input) {
+		ItemStack result = getFlightItem(input);
+		if (result == ItemStack.EMPTY) {
+			CTDMythos.logger.error("Could not remove: " + input
+					+ " from the Flight Wand registry as it doesn't exist.");
+			return;
+		}
+		this.flightItemsList.remove(input);
+		this.flightMultiList.remove(input);
+		this.flightResistList.remove(input);
 	}
 }
