375b3689efe0f6b05c60b8118ddc8599d3b97c4a
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-02-28 20:47:00 -0500

    Remove JEI Description

--- src/main/java/com/themastergeneral/ctdmythos/integration/MythosJEI.java
+++ src/main/java/com/themastergeneral/ctdmythos/integration/MythosJEI.java
@@ -23,8 +23,6 @@ public class MythosJEI implements IModPlugin {
 				"jei.xptome.desc");
 		registry.addDescription(new ItemStack(ModItems.archeron_ingot),
 				"jei.archeron_ingot.desc");
-		registry.addDescription(new ItemStack(ModItems.flight_wand),
-				"jei.flight_wand.desc");
 		registry.addDescription(new ItemStack(ModItems.teleport_wand),
 				"jei.teleport_wand.desc");
 		registry.addDescription(new ItemStack(ModItems.crystal_ender),
