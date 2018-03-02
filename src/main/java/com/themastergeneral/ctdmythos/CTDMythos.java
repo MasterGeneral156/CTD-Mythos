88780628f3980096802c5fdd81e3ebbd643ee245
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-03-01 22:28:15 -0500

    IMC Handler.

--- src/main/java/com/themastergeneral/ctdmythos/CTDMythos.java
+++ src/main/java/com/themastergeneral/ctdmythos/CTDMythos.java
@@ -7,19 +7,21 @@ import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInterModComms.IMCEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import org.apache.logging.log4j.Logger;
 
 import com.themastergeneral.ctdmythos.client.MythosTab;
+import com.themastergeneral.ctdmythos.integration.imc.IMCHandler;
 import com.themastergeneral.ctdmythos.proxy.Common;
 
 @Mod(modid = CTDMythos.MODID, name = CTDMythos.MODNAME, version = CTDMythos.VERSION, acceptedMinecraftVersions = CTDMythos.acceptedMinecraftVersions, updateJSON = CTDMythos.updateJSON, certificateFingerprint = CTDMythos.certificateFingerprint, dependencies = CTDMythos.DEPENDENCIES)
 public class CTDMythos {
 	public static final String MODID = "ctdmythos";
 	public static final String MODNAME = "CTD Mythos";
-	public static final String VERSION = "0.0.23";
+	public static final String VERSION = "0.0.24";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Mythos.json";
 	public static final String acceptedMinecraftVersions = "1.12.2";
 	public static final String certificateFingerprint = "1cd8befc36d6dedc5601d77a013f43afc71f899f";
@@ -54,5 +56,11 @@ public class CTDMythos {
 	@EventHandler
 	public void onFingerprintViolation(FMLFingerprintViolationEvent e) {
 		FMLLog.warning("Invalid fingerprint detected for CTD Mythos! TheMasterGeneral will not support this version!");
+	}
+
+	@EventHandler
+	public void handleIMC(IMCEvent e) {
+		logger.info("CTD Mythos is awaiting IMC from other mods...");
+		IMCHandler.INSTANCE.handleIMC(e.getMessages());
 	}
 }
