3aca5236a1ba324ec4f1c91ecc8cacadf155181e
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-03-06 20:58:18 -0500

    Add Crystallized Woe + Fire blocks

---
+++ src/main/java/com/themastergeneral/ctdmythos/common/blocks/CrystalBlocks.java
@@ -0,0 +1,16 @@
+package com.themastergeneral.ctdmythos.common.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class CrystalBlocks extends BaseBlock {
+
+	public CrystalBlocks(String name) {
+		super(Material.GRASS, name);
+		this.setSoundType(SoundType.GLASS);
+		this.setHardness(5.0F);
+		this.setResistance(5.0F);
+		this.setHarvestLevel("pickaxe", 3);
+	}
+
+}
--- src/main/java/com/themastergeneral/ctdmythos/common/blocks/ModBlocks.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/blocks/ModBlocks.java
@@ -17,6 +17,9 @@ public class ModBlocks extends RegisterBlock {
 
 	public static BrickBlocks crystal_fire_brick;
 	public static BricksStairs crystal_fire_stairs;
+	
+	public static CrystalBlocks crystal_woe_block;
+	public static CrystalBlocks crystal_fire_block;
 
 	public static void loadBlocks() {
 		crystal_fire_ore = register(new OreBlock(Material.SAND,
@@ -32,5 +35,8 @@ public class ModBlocks extends RegisterBlock {
 
 		crystal_fire_brick = register(new BrickBlocks("crystal_fire_brick"));
 		crystal_fire_stairs = register(new BricksStairs("crystal_fire_stairs", crystal_fire_brick.getDefaultState()));
+		
+		crystal_woe_block = register(new CrystalBlocks("crystal_woe_block"));
+		crystal_fire_block = register(new CrystalBlocks("crystal_fire_block"));
 	}
 }
