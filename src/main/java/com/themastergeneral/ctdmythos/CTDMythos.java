92403016f90a4693b661e3ab6f88873f4a08bbf9
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-04-04 18:40:20 -0400

    Network stuff

--- src/main/java/com/themastergeneral/ctdmythos/CTDMythos.java
+++ src/main/java/com/themastergeneral/ctdmythos/CTDMythos.java
@@ -10,11 +10,16 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms.IMCEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
 
 import org.apache.logging.log4j.Logger;
 
 import com.themastergeneral.ctdmythos.client.MythosTab;
 import com.themastergeneral.ctdmythos.integration.imc.IMCHandler;
+import com.themastergeneral.ctdmythos.network.PacketRequestUpdatePedestal;
+import com.themastergeneral.ctdmythos.network.PacketUpdatePedestal;
 import com.themastergeneral.ctdmythos.proxy.Common;
 
 @Mod(modid = CTDMythos.MODID, name = CTDMythos.MODNAME, version = CTDMythos.VERSION, acceptedMinecraftVersions = CTDMythos.acceptedMinecraftVersions, updateJSON = CTDMythos.updateJSON, certificateFingerprint = CTDMythos.certificateFingerprint, dependencies = CTDMythos.DEPENDENCIES)
@@ -40,11 +45,16 @@ public class CTDMythos {
 	@SidedProxy(clientSide = "com.themastergeneral.ctdmythos.proxy.Client", serverSide = "com.themastergeneral.ctdmythos.proxy.Server")
 	public static Common proxy;
 	public static Logger logger;
+	public static SimpleNetworkWrapper wrapper;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
 		logger = e.getModLog();
+		wrapper = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
+		wrapper.registerMessage(new PacketUpdatePedestal.Handler(), PacketUpdatePedestal.class, 0, Side.CLIENT);
+		wrapper.registerMessage(new PacketRequestUpdatePedestal.Handler(), PacketRequestUpdatePedestal.class, 1, Side.SERVER);
 		proxy.preInit(e);
+		proxy.registerRenderers();
 	}
 
 	@EventHandler
