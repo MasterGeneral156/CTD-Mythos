657fcc73ad06cca1bc40aa68c79a95081fb9c0f7
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-04-04 20:58:56 -0400

    Crafting on the pedestal now :)

--- src/main/java/com/themastergeneral/ctdmythos/tileentity/PedestalTileEntity.java
+++ src/main/java/com/themastergeneral/ctdmythos/tileentity/PedestalTileEntity.java
@@ -3,19 +3,23 @@ package com.themastergeneral.ctdmythos.tileentity;
 import javax.annotation.Nullable;
 
 import com.themastergeneral.ctdmythos.CTDMythos;
+import com.themastergeneral.ctdmythos.common.processing.MultiblockRecipes;
 import com.themastergeneral.ctdmythos.network.PacketRequestUpdatePedestal;
 import com.themastergeneral.ctdmythos.network.PacketUpdatePedestal;
 
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
-public class PedestalTileEntity extends TileEntity {
+public class PedestalTileEntity extends TileEntity implements ITickable {
 
 	public ItemStackHandler inventory = new ItemStackHandler(1) {
 		protected void onContentsChanged(int slot) {
@@ -26,11 +30,13 @@ public class PedestalTileEntity extends TileEntity {
 		}
 	};
 	public long lastChangeTime;
+	public int tickup;
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
 		compound.setTag("inventory", inventory.serializeNBT());
 		compound.setLong("lastChangeTime", lastChangeTime);
+		compound.setInteger("tickup", tickup);
 		return super.writeToNBT(compound);
 	}
 	
@@ -38,6 +44,7 @@ public class PedestalTileEntity extends TileEntity {
 	public void readFromNBT(NBTTagCompound compound) {
 		inventory.deserializeNBT(compound.getCompoundTag("inventory"));
 		lastChangeTime = compound.getLong("lastChangeTime");
+		tickup = compound.getInteger("tickup");
 		super.readFromNBT(compound);
 	}
 	
@@ -63,5 +70,60 @@ public class PedestalTileEntity extends TileEntity {
 	public AxisAlignedBB getRenderBoundingBox() {
 		return new AxisAlignedBB(getPos(), getPos().add(1, 2, 1));
 	}
+	
+	public int getTicks()
+	{
+		return tickup;
+	}
+	public void incrementTicks()
+	{
+		tickup++;
+		markDirty();
+	}
+	public void decrementTicks()
+	{
+		tickup--;
+		markDirty();
+	}
+	public void resetTicks()
+	{
+		tickup=0;
+		markDirty();
+	}
+
+	@Override
+	public void update() {
+		if (validItem())
+		{
+			incrementTicks();
+			if (getTicks() == 200*inventory.getStackInSlot(0).getCount())
+			{
+				int newtotal = inventory.getStackInSlot(0).getCount()*getOutput().getCount();
+				ItemStack Output = new ItemStack(getOutput().getItem(), newtotal, getOutput().getMetadata());
+				world.addWeatherEffect(new EntityLightningBolt(world,
+						pos.getX(), pos.getY() + 1, pos.getZ(), false));
+				this.inventory.setStackInSlot(0, getOutput());
+				resetTicks();
+			}
+		}
+		else
+		{
+			resetTicks();
+		}
+	}
+	
+	public boolean validItem()
+	{
+		if (MultiblockRecipes.instance().getRecipeResult(this.inventory.getStackInSlot(0)).getItem() != ItemStack.EMPTY.getItem()) 
+			return true;
+		else
+			return false;
+	}
+	
+	public ItemStack getOutput()
+	{
+		return MultiblockRecipes.instance().getRecipeResult(this.inventory.getStackInSlot(0));
+	}
+
 
 }
