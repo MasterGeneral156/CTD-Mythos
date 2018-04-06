914cea43d548a5bbfe80b98d95fa52427b89da67
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-04-06 15:39:57 -0400

    Add creation method

--- src/main/java/com/themastergeneral/ctdmythos/common/items/crystals/CrystallizedOath.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/crystals/CrystallizedOath.java
@@ -1,15 +1,22 @@
 package com.themastergeneral.ctdmythos.common.items.crystals;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
+import com.themastergeneral.ctdmythos.common.blocks.ModBlocks;
 import com.themastergeneral.ctdmythos.common.config.ModConfig;
 import com.themastergeneral.ctdmythos.common.items.ModItems;
 import com.themastergeneral.ctdmythos.common.items.misc.BaseItem;
@@ -18,6 +25,7 @@ import com.themastergeneral.ctdmythos.common.processing.ModSounds;
 
 public class CrystallizedOath extends BaseItem {
 
+	private Block containedBlock;
 	public CrystallizedOath(String name) {
 		super(name);
 	}
@@ -48,6 +56,41 @@ public class CrystallizedOath extends BaseItem {
 					worldIn.playSound(playerIn, playerIn.getPosition(),
 							ModSounds.spell_complete, SoundCategory.PLAYERS,
 							1.0F, 1.0F);
+				}
+			}
+		}
+		if (playerIn.isSneaking()) {
+			Block blocktotest = Blocks.BRICK_BLOCK;
+			boolean flag = this.containedBlock == blocktotest;
+			RayTraceResult raytraceresult = this.rayTrace(worldIn, playerIn,
+					flag);
+			if (raytraceresult == null) {
+				return new ActionResult(EnumActionResult.PASS, mainhand);
+			} else if (raytraceresult.typeOfHit != RayTraceResult.Type.BLOCK) {
+				return new ActionResult(EnumActionResult.PASS, mainhand);
+			} else {
+				BlockPos blockpos = raytraceresult.getBlockPos();
+				if (!worldIn.isBlockModifiable(playerIn, blockpos)) {
+					return new ActionResult(EnumActionResult.FAIL, mainhand);
+				}
+				if (!playerIn.canPlayerEdit(
+						blockpos.offset(raytraceresult.sideHit),
+						raytraceresult.sideHit, mainhand)) {
+					return new ActionResult(EnumActionResult.FAIL, mainhand);
+				} else {
+					IBlockState iblockstate = worldIn.getBlockState(blockpos);
+					if (iblockstate == blocktotest.getDefaultState()) {
+						worldIn.setBlockState(blockpos,
+								ModBlocks.crystal_oath_brick.getDefaultState(),
+								11);
+						EntityLightningBolt lightning = new EntityLightningBolt(
+								worldIn, blockpos.getX(), blockpos.getY(),
+								blockpos.getZ(), false);
+						worldIn.addWeatherEffect(lightning);
+						return new ActionResult(EnumActionResult.PASS, mainhand);
+					} else {
+						return new ActionResult(EnumActionResult.FAIL, mainhand);
+					}
 				}
 			}
 		}
