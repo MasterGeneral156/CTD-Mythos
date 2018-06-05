654d099add74e987564a6d1b58880d7e7d079871
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-06-04 22:43:39 -0400

    Stackable!

--- src/main/java/com/themastergeneral/ctdmythos/common/items/baubles/TrinketHealth.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/baubles/TrinketHealth.java
@@ -1,5 +1,7 @@
 package com.themastergeneral.ctdmythos.common.items.baubles;
 
+import com.themastergeneral.ctdmythos.common.config.ModConfig;
+
 import baubles.api.BaubleType;
 import baubles.api.inv.BaublesInventoryWrapper;
 import net.minecraft.entity.EntityLivingBase;
@@ -7,39 +9,41 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-public class TrinketHealth extends BasicBauble {
-	
-	public TrinketHealth(String name) {
-		super(name);
-		this.maxStackSize=1;
-	}
-	
-	@Override
-	public BaubleType getBaubleType(ItemStack arg0) {
-		return BaubleType.CHARM;
-	}
-	
-	@Override
-	public void onEquipped(ItemStack itemstack, EntityLivingBase entity)
-	{
-		EntityPlayer player = (EntityPlayer) entity;
-		double hp = player.getMaxHealth()+10F;
-		player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(hp);
-	}
-	
-	@Override
-	public void onUnequipped(ItemStack itemstack, EntityLivingBase entity)
-	{
-		EntityPlayer player = (EntityPlayer) entity;
-		double hp = player.getMaxHealth()-10F;
-		player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(hp);
-	}	
+public class TrinketHealth extends BasicBauble
+{
 
-	@Override
-	public void onWornTick(ItemStack itemstack, EntityLivingBase player)
-	{
-		if (player.getMaxHealth() != 30F)
-			player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30F);
-	}
+    public TrinketHealth(String name)
+    {
+        super(name);
+        this.maxStackSize = ModConfig.heart_stack;
+    }
+
+    @Override
+    public BaubleType getBaubleType(ItemStack arg0)
+    {
+        return BaubleType.CHARM;
+    }
+
+    @Override
+    public void onEquipped(ItemStack itemstack, EntityLivingBase entity)
+    {
+        EntityPlayer player = (EntityPlayer) entity;
+        double hp = player.getMaxHealth() + (itemstack.getCount() * 10F);
+        player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(hp);
+    }
+
+    @Override
+    public void onUnequipped(ItemStack itemstack, EntityLivingBase entity)
+    {
+        EntityPlayer player = (EntityPlayer) entity;
+        double hp = player.getMaxHealth() - (itemstack.getCount() * 10F);
+        player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(hp);
+    }
+
+    @Override
+    public void onWornTick(ItemStack itemstack, EntityLivingBase player)
+    {
+        if (player.getMaxHealth() != (20F + itemstack.getCount() * 10F))
+            player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20F + (itemstack.getCount() * 10F));
+    }
 }
-
