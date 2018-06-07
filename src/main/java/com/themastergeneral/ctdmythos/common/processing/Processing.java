eda72934bcd193fa5c6240cf7f4d3abcc7f1fbd2
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-06-06 22:07:52 -0400

    Previous

--- src/main/java/com/themastergeneral/ctdmythos/common/processing/Processing.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/processing/Processing.java
@@ -11,15 +11,15 @@ public class Processing
     public static void initProcessing()
     {
         Handlers.addCrystalRecipe(new ItemStack(ModBlocks.crystal_fire_ore),
-                new ItemStack(ModItems.crystal_fire, 3), 0F);
+                new ItemStack(ModItems.crystal_fire, 1), 0F);
         Handlers.addCrystalRecipe(new ItemStack(ModBlocks.crystal_woe_ore),
-                new ItemStack(ModItems.crystal_woe, 3), 0F);
+                new ItemStack(ModItems.crystal_woe, 1), 0F);
         Handlers.addCrystalRecipe(new ItemStack(ModBlocks.crystal_memory_ore),
-                new ItemStack(ModItems.crystal_memory, 3), 0F);
+                new ItemStack(ModItems.crystal_memory, 1), 0F);
         Handlers.addCrystalRecipe(new ItemStack(ModBlocks.crystal_grief_ore),
-                new ItemStack(ModItems.crystal_grief, 3), 0F);
+                new ItemStack(ModItems.crystal_grief, 1), 0F);
         Handlers.addCrystalRecipe(new ItemStack(ModBlocks.crystal_oath_ore),
-                new ItemStack(ModItems.crystal_oath, 3), 0F);
+                new ItemStack(ModItems.crystal_oath, 1), 0F);
 
         // Enderfuge only
         Handlers.addEnderfugeRecipe(new ItemStack(ModItems.grimshears, 1,
