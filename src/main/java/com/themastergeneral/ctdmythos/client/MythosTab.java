d42fcf0cfd6cfa785cb65ca44674b01ecb98c027
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-06-10 11:42:07 -0400

    Change creative tab icon

--- src/main/java/com/themastergeneral/ctdmythos/client/MythosTab.java
+++ src/main/java/com/themastergeneral/ctdmythos/client/MythosTab.java
@@ -5,6 +5,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 import com.themastergeneral.ctdmythos.CTDMythos;
+import com.themastergeneral.ctdmythos.common.blocks.ModBlocks;
 import com.themastergeneral.ctdmythos.common.items.ModItems;
 
 public class MythosTab extends CreativeTabs
@@ -18,7 +19,7 @@ public class MythosTab extends CreativeTabs
     @Override
     public ItemStack getTabIconItem()
     {
-        return new ItemStack(ModItems.flight_wand);
+        return new ItemStack(ModBlocks.pedestal_block);
     }
 
     @Override
