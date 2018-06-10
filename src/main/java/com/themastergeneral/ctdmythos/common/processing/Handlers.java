dfc550627b2d725e04bb34812df0b113d9c96625
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-06-10 11:53:09 -0400

    Remove this

--- src/main/java/com/themastergeneral/ctdmythos/common/processing/Handlers.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/processing/Handlers.java
@@ -1,7 +1,5 @@
 package com.themastergeneral.ctdmythos.common.processing;
 
-import com.themastergeneral.ctdmythos.integration.EnderfugeIMC;
-
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -13,12 +11,12 @@ public class Handlers
             float xp)
     {
         // Enderfuge stuff if its available? /shrug.
-        EnderfugeIMC.addEnderfugeSmelt(input, new ItemStack(output.getItem(), output.getCount()*3, output.getMetadata()), xp);
+        IMCHelper.addEnderfugeSmelt(input, new ItemStack(output.getItem(), output.getCount()*3, output.getMetadata()), xp);
         GameRegistry.addSmelting(input, output, xp);
     }
 
     public static void addEnderfugeRecipe(ItemStack input, ItemStack output)
     {
-        EnderfugeIMC.addEnderfugeSmelt(input, output, 0F);
+        IMCHelper.addEnderfugeSmelt(input, output, 0F);
     }
 }
