19f5a5472ededdf71b789817ec45999c4035213e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2018-12-19 12:46:04 -0500

    owo

---
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/artifacts/LightningStaff.java
@@ -0,0 +1,37 @@
+package com.themastergeneral.ctdmythos.common.items.artifacts;
+
+import com.themastergeneral.ctdmythos.common.items.ModItems;
+import com.themastergeneral.ctdmythos.common.items.misc.BaseItem;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+
+public class LightningStaff extends BaseItem {
+
+	public LightningStaff(String name) {
+		super(name);
+		this.setMaxStackSize(1);
+		this.setNoRepair();
+	}
+
+	@Override
+    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+    {
+		World worldIn=target.getEntityWorld();
+		if (attacker instanceof EntityPlayer)
+		{
+			EntityPlayer attackerr = (EntityPlayer) attacker;
+			stack.shrink(1);
+			attackerr.addItemStackToInventory(new ItemStack(ModItems.enchanted_stick, 1));
+		}
+		worldIn.addWeatherEffect(new EntityLightningBolt(worldIn, target.posX, target.posY, target.posZ, false));
+		target.attackEntityFrom(DamageSource.LIGHTNING_BOLT, Float.MAX_VALUE);
+		//Maybe beat draconic?
+		target.attackEntityFrom(DamageSource.OUT_OF_WORLD, Float.MAX_VALUE);
+		return true;
+    }
+}
