1736231bbab88407f86a3489e8b52ef036da61b6
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-11-11 20:25:35 -0500

    Mythos Inventory Generators.

---
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/mythos/MythosInventGen.java
@@ -0,0 +1,44 @@
+package com.themastergeneral.ctdmythos.common.items.mythos;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class MythosInventGen extends MythosItemBase {
+
+	public int mpt;
+	public MythosInventGen(String name, int mythosPerTick) {
+		super(name);
+		mpt = mythosPerTick;
+	}
+	@Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
+    {
+		if (entityIn instanceof EntityLivingBase)
+        {
+            EntityLivingBase entitylive = (EntityLivingBase) entityIn;
+            EntityPlayer playerIn = (EntityPlayer) entityIn;
+            addMythos(playerIn, mpt);
+        }
+    }
+	// Add tooltip on client side.
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn,
+            List<String> tooltip, ITooltipFlag flagIn)
+    {
+    	tooltip.add("Generates " + mpt + " mythos / tick.");
+    }
+
+}
