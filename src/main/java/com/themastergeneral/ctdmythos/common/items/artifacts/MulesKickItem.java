2a0edc29cbbd41b6ee0515dc9983badc26fc76cc
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-11-11 21:13:20 -0500

    Mules kick uses mythos.

--- src/main/java/com/themastergeneral/ctdmythos/common/items/artifacts/MulesKickItem.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/artifacts/MulesKickItem.java
@@ -8,6 +8,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
 import com.themastergeneral.ctdmythos.CTDMythos;
@@ -22,7 +23,6 @@ public class MulesKickItem extends BaseItem
     {
         super(name);
         this.maxStackSize = 1;
-        this.setMaxDamage(ModConfig.mule_damage+1);
     }
 
     // When the Mule's Kick is used, give the player Speed 5 for 10 Seconds.
@@ -31,16 +31,25 @@ public class MulesKickItem extends BaseItem
     public ActionResult<ItemStack> onItemRightClick(World worldIn,
             EntityPlayer playerIn, EnumHand handIn)
     {
-        if (!worldIn.isRemote)
-        {
-            ItemStack ItemStack = playerIn.getHeldItem(handIn);
-            playerIn.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 4,
-                    true, false));
-            ItemStack.damageItem(1, playerIn);
-            playerIn.getCooldownTracker().setCooldown(this, 200);
-        }
-        worldIn.playSound(playerIn, playerIn.getPosition(),
+    	int playerMythos = getMythos(playerIn);
+		if (checkMythos(playerMythos, ModConfig.mythosCostMulesKick))
+		{
+			removeMythos(playerIn, ModConfig.mythosCostMulesKick);
+	        if (!worldIn.isRemote)
+	        {
+	            ItemStack ItemStack = playerIn.getHeldItem(handIn);
+	            playerIn.addPotionEffect(new PotionEffect(MobEffects.SPEED, 200, 4,
+	                    true, false));
+	            playerIn.getCooldownTracker().setCooldown(this, 200);
+	        }
+	        worldIn.playSound(playerIn, playerIn.getPosition(),
                 ModSounds.mules_kick, SoundCategory.PLAYERS, 1.0F, 1.0F);
+		}
+		else
+		{
+			playerIn.sendStatusMessage(new TextComponentTranslation(
+	                "You need at least " + ModConfig.mythosCostMulesKick + " mythos to use the Mule's Kick."),true);
+		}
         return new ActionResult<ItemStack>(EnumActionResult.PASS,
                 playerIn.getHeldItem(handIn));
     }
