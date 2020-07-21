001ca46643309c4dfcdc3223300cc5cbc89f6a36
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-07-20 22:08:09 -0400

    Memory spawns in plains biomes

--- src/main/java/com/themastergeneral/ctdmythos/common/items/crystals/CrystallizedMemory.java
+++ src/main/java/com/themastergeneral/ctdmythos/common/items/crystals/CrystallizedMemory.java
@@ -5,6 +5,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -13,7 +14,9 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomePlains;
 
 import com.themastergeneral.ctdmythos.client.sound.ModSounds;
 import com.themastergeneral.ctdmythos.common.blocks.ModBlocks;
@@ -38,60 +41,69 @@ public class CrystallizedMemory extends CrystalBase
     {
         if (!worldIn.isRemote)
         {
-            ItemStack offhand = playerIn.getHeldItemOffhand();
-            ItemStack mainhand = playerIn.getHeldItemMainhand();
-            if (playerIn.isSneaking())
-            {
-                Block blocktotest = Blocks.BRICK_BLOCK;
-                boolean flag = this.containedBlock == blocktotest;
-                RayTraceResult raytraceresult = this.rayTrace(worldIn, playerIn, flag);
-                if (raytraceresult == null)
-                {
-                    return new ActionResult(EnumActionResult.PASS, mainhand);
-                }
-                else if (raytraceresult.typeOfHit != RayTraceResult.Type.BLOCK)
-                {
-                    return new ActionResult(EnumActionResult.PASS, mainhand);
-                }
-                else
-                {
-                    BlockPos blockpos = raytraceresult.getBlockPos();
-                    if (!worldIn.isBlockModifiable(playerIn, blockpos))
-                    {
-                        return new ActionResult(EnumActionResult.FAIL, mainhand);
-                    }
-                    if (!playerIn.canPlayerEdit(
-                            blockpos.offset(raytraceresult.sideHit),
-                            raytraceresult.sideHit, mainhand))
-                    {
-                        return new ActionResult(EnumActionResult.FAIL, mainhand);
-                    }
-                    else
-                    {
-                        IBlockState iblockstate = worldIn
-                                .getBlockState(blockpos);
-                        if (iblockstate == blocktotest.getDefaultState())
-                        {
-                            worldIn.setBlockState(blockpos,
-                                    ModBlocks.crystal_memory_brick
-                                            .getDefaultState(), 11);
-                            EntityLightningBolt lightning = new EntityLightningBolt(
-                                    worldIn, blockpos.getX(), blockpos.getY(),
-                                    blockpos.getZ(), false);
-                            worldIn.addWeatherEffect(lightning);
-                            mainhand.shrink(1);
-                            return new ActionResult(EnumActionResult.PASS,
-                                    mainhand);
-                        }
-                        else
-                        {
-                            return new ActionResult(EnumActionResult.FAIL,
-                                    mainhand);
-                        }
-                    }
-                }
-            }
-        }
+        	//Only work in Plains-based biomes.
+        	if (worldIn.getBiome(playerIn.getPosition()) instanceof BiomePlains)
+    		{
+	            ItemStack offhand = playerIn.getHeldItemOffhand();
+	            ItemStack mainhand = playerIn.getHeldItemMainhand();
+	            if (playerIn.isSneaking())
+	            {
+	                Block blocktotest = Blocks.BRICK_BLOCK;
+	                boolean flag = this.containedBlock == blocktotest;
+	                RayTraceResult raytraceresult = this.rayTrace(worldIn, playerIn, flag);
+	                if (raytraceresult == null)
+	                {
+	                    return new ActionResult(EnumActionResult.PASS, mainhand);
+	                }
+	                else if (raytraceresult.typeOfHit != RayTraceResult.Type.BLOCK)
+	                {
+	                    return new ActionResult(EnumActionResult.PASS, mainhand);
+	                }
+	                else
+	                {
+	                    BlockPos blockpos = raytraceresult.getBlockPos();
+	                    if (!worldIn.isBlockModifiable(playerIn, blockpos))
+	                    {
+	                        return new ActionResult(EnumActionResult.FAIL, mainhand);
+	                    }
+	                    if (!playerIn.canPlayerEdit(
+	                            blockpos.offset(raytraceresult.sideHit),
+	                            raytraceresult.sideHit, mainhand))
+	                    {
+	                        return new ActionResult(EnumActionResult.FAIL, mainhand);
+	                    }
+	                    else
+	                    {
+	                        IBlockState iblockstate = worldIn
+	                                .getBlockState(blockpos);
+	                        if (iblockstate == blocktotest.getDefaultState())
+	                        {
+	                            worldIn.setBlockState(blockpos,
+	                                    ModBlocks.crystal_memory_brick
+	                                            .getDefaultState(), 11);
+	                            EntityLightningBolt lightning = new EntityLightningBolt(
+	                                    worldIn, blockpos.getX(), blockpos.getY(),
+	                                    blockpos.getZ(), false);
+	                            worldIn.addWeatherEffect(lightning);
+	                            mainhand.shrink(1);
+	                            addMythos(playerIn,25);
+	                            return new ActionResult(EnumActionResult.PASS,
+	                                    mainhand);
+	                        }
+	                        else
+	                        {
+	                            return new ActionResult(EnumActionResult.FAIL,
+	                                    mainhand);
+	                        }
+	                    }
+	                }
+	            }
+	        }
+        	else
+        	{
+        		playerIn.sendStatusMessage(new TextComponentString("This may only be done in a Plains-based biome."), true);
+        	}
+		}
         return new ActionResult<ItemStack>(EnumActionResult.PASS,
                 playerIn.getHeldItem(handIn));
     }
