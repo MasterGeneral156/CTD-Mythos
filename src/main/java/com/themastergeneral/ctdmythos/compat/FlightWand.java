b378352a0d1745fe90b8be4c32f8da6511913139
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-08-01 00:21:38 -0400

    Fixes CT recipes not showing in JEI

--- src/main/java/com/themastergeneral/ctdmythos/compat/FlightWand.java
+++ src/main/java/com/themastergeneral/ctdmythos/compat/FlightWand.java
@@ -1,5 +1,7 @@
 package com.themastergeneral.ctdmythos.compat;
 
+import javax.annotation.Nullable;
+
 import com.themastergeneral.ctdmythos.CTDMythos;
 import com.themastergeneral.ctdmythos.common.processing.MultiblockRecipes;
 import com.themastergeneral.ctdmythos.common.processing.WandFlightItems;
@@ -15,63 +17,14 @@ import stanhebben.zenscript.annotations.ZenMethod;
 public class FlightWand 
 {
 	@ZenMethod
-    public static void addRecipe(IItemStack input, int flightTime, int flightMultiplier, int resistTime) 
+    public static void addRecipe(IItemStack input, int flightTime, @Nullable int flightMultiplier, @Nullable int resistTime) 
 	{
-		CTDMythos.LATE_ADDITIONS.add(new Add(InputHelper.toStack(input), flightTime, flightMultiplier, resistTime));
-	}
-	
-	@ZenMethod
-    public static void addRecipe(IItemStack input, int flightTime, int flightMultiplier) 
-	{
-		CTDMythos.LATE_ADDITIONS.add(new Add(InputHelper.toStack(input), flightTime, flightMultiplier, 0));
-	}
-	
-	@ZenMethod
-    public static void addRecipe(IItemStack input, int flightTime) 
-	{
-		CTDMythos.LATE_ADDITIONS.add(new Add(InputHelper.toStack(input), flightTime, 0, 0));
+		WandFlightItems.instance().addFlight(InputHelper.toStack(input), flightTime, flightMultiplier, resistTime);
 	}
 	
 	@ZenMethod
     public static void removeRecipe(IItemStack input) 
 	{
-		CTDMythos.LATE_ADDITIONS.add(new Remove(InputHelper.toStack(input)));
+		WandFlightItems.instance().removeFlight(InputHelper.toStack(input));
 	}
-	private static class Add extends BaseAction 
-	{ 
-        private ItemStack input;
-        private int flightTime;
-        private int flightMultiplier;
-        private int resistTime;
-        
-        public Add(ItemStack input, int flightTime, int flightMultiplier, int resistTime) 
-        {
-            super("WandFlightItem");
-            this.input = input;
-            this.flightTime = flightTime;
-            this.flightMultiplier = flightMultiplier;
-            this.resistTime = resistTime;
-        }
-        
-        @Override
-        public void apply() 
-        {
-        	WandFlightItems.instance().addFlight(input, flightTime, flightMultiplier, resistTime);
-        }
-    }
-	private static class Remove extends BaseAction 
-	{
-        private ItemStack input;
-        
-        public Remove(ItemStack input) {
-            super("WandFlightItem");
-            this.input = input;
-        }
-        
-        @Override
-        public void apply() 
-        {
-           WandFlightItems.instance().removeFlight(input);
-        }
-    }
 }
--- src/main/java/com/themastergeneral/ctdmythos/compat/Pedestal.java
+++ src/main/java/com/themastergeneral/ctdmythos/compat/Pedestal.java
@@ -16,49 +16,13 @@ public class Pedestal
 	@ZenMethod
     public static void addRecipe(IItemStack input, IItemStack output) 
 	{
-		CTDMythos.LATE_ADDITIONS.add(new Add(InputHelper.toStack(input), InputHelper.toStack(output)));
+		MultiblockRecipes.instance().addRecipe(InputHelper.toStack(input), InputHelper.toStack(output));
 	}
 	
 	@ZenMethod
     public static void removeRecipe(IItemStack input, IItemStack output) 
 	{
-		CTDMythos.LATE_ADDITIONS.add(new Remove(InputHelper.toStack(input), InputHelper.toStack(output)));
+		MultiblockRecipes.instance().removeRecipe(InputHelper.toStack(input), InputHelper.toStack(output));
 	}
-	
-	private static class Add extends BaseAction 
-	{ 
-        private ItemStack output;
-        private ItemStack input;
-        
-        public Add(ItemStack input, ItemStack output) 
-        {
-            super("Pedestal");
-            this.output = output;
-            this.input = input;
-        }
-        
-        @Override
-        public void apply() 
-        {
-        	MultiblockRecipes.instance().addRecipe(input, output);
-        }
-    }
-	private static class Remove extends BaseAction 
-	{
-        private ItemStack input;
-        private ItemStack output;
-        
-        public Remove(ItemStack input, ItemStack output) {
-            super("Pedestal");
-            this.input = input;
-            this.input = output;
-        }
-        
-        @Override
-        public void apply() 
-        {
-           MultiblockRecipes.instance().removeRecipe(input, output);
-        }
-    }
 	
 }
