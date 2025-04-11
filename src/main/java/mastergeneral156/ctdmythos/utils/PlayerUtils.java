b0e885a0ed837163b2077bdd45f0e0e7d536a9e3
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-04-11 04:55:41 -0400

    Create PlayerUtils.java

---
+++ src/main/java/mastergeneral156/ctdmythos/utils/PlayerUtils.java
@@ -0,0 +1,21 @@
+package mastergeneral156.ctdmythos.utils;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+
+public class PlayerUtils {
+
+    public static void grantAdvancement(ServerPlayer player, ResourceLocation advancementId) {
+        Advancement advancement = player.server.getAdvancements().getAdvancement(advancementId);
+        if (advancement != null) {
+            AdvancementProgress progress = player.getAdvancements().getOrStartProgress(advancement);
+            if (!progress.isDone()) {
+                for (String criterion : progress.getRemainingCriteria()) {
+                    player.getAdvancements().award(advancement, criterion);
+                }
+            }
+        }
+    }
+}
